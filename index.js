39b16c212a561706fc6d24fb3d7d88c73ebf04f6
“Liam <okeefe.liam@yahoo.com>
2022-08-30 22:13:05 -0500

    commit tree 0b92eedcc07613c060ce8b083790b17e16b3f97a

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2
+
+divide(2000,100)
+
+let square = () => ("square")
+
+const sum = (parameter1, parameter2) => {
+  console.log(`Adding ${parameter1}`);
+  console.log(`Adding ${parameter2}`);
+  return parameter1 + parameter2;
+}
+
+let add = () => ("add")
